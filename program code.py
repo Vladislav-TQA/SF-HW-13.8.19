d03ebe653c2554aa3c3426aba28d789b8313da93
Vlad <110028579+Vladislav-TQA@users.noreply.github.com>
2022-08-18 11:43:25 +0700

    Update program code.py

--- program code.py
+++ program code.py
@@ -1,14 +1,25 @@
-ticket = int(input('Введите количество билетов: '))
 cost = 0.0
+while True:
+    try:
+        ticket = int(input('Введите количество билетов: '))
+        if type(ticket) == int:
+            break
+    except ValueError:
+        print('Введите целое число')
 for i in range(ticket):
-    age = int(input('Укажите возраст: '))
-    if age < 18:
-        cost = cost + 0
-    elif 18 <= age < 25:
-        cost = cost + 990
-    else:
-        cost = cost + 1390
+    while True:
+        try:
+            age = int(input('Укажите возраст: '))
+            if age < 18:
+                cost += 0
+            elif 18 <= age < 25:
+                cost += 990
+            else:
+                cost = cost + 1390
+            if type(age) == int:
+                break
+        except ValueError:
+            print('Укажите корректный возраст: ')
 if ticket > 3:
     cost = cost * 0.9
-
 print('Итого: ' + str('%.2f' % round(cost)) + ' ' + 'руб')
