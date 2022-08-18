165745bba1dba0b14aefb6eb85c10f8895b062d0
Vlad <110028579+Vladislav-TQA@users.noreply.github.com>
2022-08-18 10:54:43 +0700

    Update program code.py

--- program code.py
+++ program code.py
@@ -6,7 +6,9 @@ for i in range(ticket):
         cost = cost + 0
     elif 18 <= age < 25:
         cost = cost + 990
-    else: cost = cost + 1390
-    if ticket > 3:
-        cost = cost * 0.9
-print('Итого:'+ ' ' + str('%.2f' % round(cost)) + ' ' + 'руб')
+    else:
+        cost = cost + 1390
+if ticket > 3:
+    cost = cost * 0.9
+
+print('Итого: ' + str('%.2f' % round(cost)) + ' ' + 'руб')
